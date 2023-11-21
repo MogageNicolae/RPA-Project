d1ac6b807e1b26c98b6e406473cb32aed596688b
Nicolae Mogage <mogagenicolae@yahoo.com>
2023-11-21 12:41:32 +0200

    Added comments classification

--- Youtube/.local/install/CodedWorkflow.cs
+++ Youtube/.local/install/CodedWorkflow.cs
@@ -12,7 +12,6 @@ using UiPath.Testing.Enums;
 using UiPath.UIAutomationNext.API.Contracts;
 using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
-using UiPath.CodedWorkflows.DescriptorIntegration;
 
 namespace Youtube
 {
@@ -20,45 +19,13 @@ namespace Youtube
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
-        protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
+        protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
 
-        protected UiPath.Testing.API.ITestingService testing { get => serviceContainer.Resolve<UiPath.Testing.API.ITestingService>(); }
+        protected UiPath.Testing.API.ITestingService testing { get => serviceContainer.Resolve<UiPath.Testing.API.ITestingService>() ; }
 
-        protected UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService uiAutomation { get => serviceContainer.Resolve<UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService>(); }
-    }
-}
-
-namespace Youtube.ObjectRepository
-{
-    public static class Descriptors
-    {
-    }
-}
-
-namespace Youtube._Implementation
-{
-    internal class ScreenDescriptorDefinition : IScreenDescriptorDefinition
-    {
-        public IScreenDescriptor Screen { get; set; }
-
-        public string Reference { get; set; }
-
-        public string DisplayName { get; set; }
-    }
-
-    internal class ElementDescriptorDefinition : IElementDescriptorDefinition
-    {
-        public IScreenDescriptor Screen { get; set; }
-
-        public string Reference { get; set; }
-
-        public string DisplayName { get; set; }
-
-        public IElementDescriptor ParentElement { get; set; }
-
-        public IElementDescriptor Element { get; set; }
+        protected UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService uiAutomation { get => serviceContainer.Resolve<UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService>() ; }
     }
 }
